d7b752206cf4f3fcce20d44f9d6db6f81930dc91
Reagan Mahinay <reaganmahinay.web@gmail.com>
2025-08-27 01:33:41 +0800

    Update Tailwind CSS configuration to include additional content paths and define theme settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   prefix: 'rgn-', // <- prefix should be placed **before** `content`
-  content: ['./templates/**/*.php', './admin/templates/**/*.php'],
-  theme: {},
+  content: ['./templates/**/*.php', './admin/templates/**/*.php', './includes/*.php'],
+  theme: {
+    extend: {
+      fontSize: {
+        base: '0.90rem'
+      }
+    }
+  },
   plugins: [],
 }
