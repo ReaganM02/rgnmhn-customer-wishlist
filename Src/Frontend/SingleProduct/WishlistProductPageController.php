47b5c1a976eb7a1165f46d91f4c69ef549a588b2
Reagan Mahinay <reaganmahinay.web@gmail.com>
2025-08-28 07:12:54 +0800

    Implement AJAX handler for retrieving customer wishlist data and enhance product ID validation

--- Src/Frontend/SingleProduct/WishlistProductPageController.php
+++ Src/Frontend/SingleProduct/WishlistProductPageController.php
@@ -20,6 +20,40 @@ class WishlistProductPageController
   {
     add_action('wp_ajax_rgn_add_customer_wishlist', [$this, 'add']);
     add_action('wp_ajax_nopriv_rgn_add_customer_wishlist', [$this, 'add']);
+
+    add_action('wp_ajax_rgn_customer_wishlist_get_data', [$this, 'get']);
+    add_action('wp_ajax_nopriv_rgn_customer_wishlist_get_data', [$this, 'get']);
+  }
+
+  public function get()
+  {
+    // Prevent any caching layer from storing the response
+    nocache_headers();
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+
+    check_ajax_referer('rgn_get_customer_wishlist_security', 'security');
+    $singleProduct = new WishlistProductPage();
+
+    $productID = $_POST['product-id'] ?? 0;
+
+    if (!$productID) {
+      wp_send_json_error('Invalid product ID');
+    }
+
+    $product = wc_get_product($productID);
+
+    if ($product->is_type('variable')) {
+      $addedIds =  $singleProduct->getAddedVariationIDs($productID);
+      wp_send_json_success($addedIds);
+    }
+
+    if ($product->is_type('simple')) {
+      $isAdded  = $singleProduct->isAdded($productID);
+      wp_send_json_success($isAdded);
+    }
+
+    wp_send_json_success($product->get_id());
   }
 
   public function add()
